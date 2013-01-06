a1b82803f3bba99b07249d021a11109ed1997de7
Diwa del Mundo <diwadm@gmail.com>
2013-01-07 00:06:17 +0800

    Added code for loading middle wares in application.rb.

--- lib/generators/headshot/config/config_generator.rb
+++ lib/generators/headshot/config/config_generator.rb
@@ -17,6 +17,27 @@ module Headshot
 
         copy_file('../middlewares/flash_session_cookie_middleware.rb', 'app/middlewares/flash_session_cookie_middleware.rb')
       end
+
+      def load_middlewares_in_application_rb
+        application_rb_path = "config/application.rb"
+        code_marker = "class Application < Rails::Application"
+        loading_code = 'config.autoload_paths += %W(#{config.root}/app/middlewares)'
+        say_status('checking', 'Checking if application.rb contains the loading code for the middleware.')
+
+        application_rb = IO.readlines(application_rb_path)
+        application_rb_lines = application_rb.join('')
+        # application_rb_lines =~ /config\.autoload_paths \+= \%W\(\#\{config\.root\}\/app\/middlewares\)/
+        if application_rb_lines.include?(loading_code)
+          say('Middlewares are being loaded already.')
+        else
+          say_status('modifying', 'Inserting code for loading middlewares.')
+          application_rb_lines.sub!(code_marker, "#{code_marker}\n\t\t#{loading_code}")
+
+          application_rb_file = File.open(application_rb_path, "w")
+          application_rb_file.write(application_rb_lines)
+          application_rb_file.close
+        end
+      end
     end
   end
 end
