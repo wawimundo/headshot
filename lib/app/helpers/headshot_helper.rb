a6032a954d17922c5b318ee7692d0e3b4546e939
Diwa del Mundo <diwadm@gmail.com>
2013-01-07 04:31:11 +0800

    Removed duplicate method for file name generation.

--- lib/app/helpers/headshot_helper.rb
+++ lib/app/helpers/headshot_helper.rb
@@ -1,9 +1,4 @@
 module HeadshotHelper
-  def headshot_file_path
-    file_name = "headshot_capture_#{rand(10000)}_#{Time.now.to_i}.jpg"
-    File.join(Rails.root, 'public', 'headshots', file_name)
-  end
-
   def headshot_snap_button(label = 'Snap', default_handler = 'headshotHandlers.snap()')
     button_tag(label, :onclick => default_handler)
   end
